d7591068494fb3ec9bf0a2e5428c4f26987b78d5
IrinaB <balkarovairina@gmail.com>
2024-01-09 21:07:40 +0100

    Add Password Encoder

--- server/src/main/java/com/eazybytes/server/config/SecurityConfig.java
+++ server/src/main/java/com/eazybytes/server/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -27,13 +28,8 @@ public class SecurityConfig {
     }
 
 
-/*    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
-    }*/
-
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- server/src/main/java/com/eazybytes/server/controller/LoginController.java
+++ server/src/main/java/com/eazybytes/server/controller/LoginController.java
@@ -4,17 +4,19 @@ import com.eazybytes.server.model.Customer;
 import com.eazybytes.server.repository.CustomerRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
-    final
-    CustomerRepository customerRepository;
+    final CustomerRepository customerRepository;
+    final PasswordEncoder passwordEncoder;
 
-    public LoginController(CustomerRepository customerRepository) {
+    public LoginController(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
         this.customerRepository = customerRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/register")
@@ -22,6 +24,8 @@ public class LoginController {
         Customer savedCustomer = null;
         ResponseEntity<String> response = null;
         try {
+            String hashedPassword = passwordEncoder.encode(customer.getPassword());
+            customer.setPassword(hashedPassword);
             savedCustomer = customerRepository.save(customer);
             if (savedCustomer.getId() > 0) {
                 response = ResponseEntity
